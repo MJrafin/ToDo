6fc7aaf500c49bf952578e5b11a1a2181817a7ef
Rahemul Islam <rahemul.islam@northsouth.edu>
2021-09-29 20:55:41 +0600

    registration form updated

--- database/migrations/2021_09_28_143751_creat_user_table.php
+++ database/migrations/2021_09_28_143751_creat_user_table.php
@@ -13,7 +13,11 @@ class CreatUserTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
     }
 
     /**
